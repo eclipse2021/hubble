0623466750f5c20a0579e02fd34b367145618713
eclipse2021 <eclupse2021pdhs@gmail.com>
2022-05-08 12:27:54 +0900

    '^'

--- model.py
+++ model.py
@@ -4,24 +4,23 @@ class Linear:
     def  __init__(self):
         self.h_0 = random.uniform(-1, 1)
         self.grad_tape = []
-        self.lr = 0.01
+        self.lr = 0.00297
 
     def forward(self, x):
         y = self.h_0 * x
-        self.grad_tape.append(x)
+        #self.grad_tape.append(x)
         return y
 
     def MSE(self, prediction, y):
         #error = numpy.power(prediction - y, 2) * (1/2)
-        error = ((prediction - y) ** 2) * 0.5
         self.grad_tape.append(prediction - y)
-        return error
+        #return error
 
     def zero_grad(self):
         self.grad_tape = []
 
     def backward(self):
-        self.h_0 -= self.lr * self.grad_tape[0] * self.grad_tape[1]
+        self.h_0 -= self.lr * self.grad_tape[0]
 
     def summary(self):
         print(self.h_0)
--- optimize.py
+++ optimize.py
@@ -3,7 +3,7 @@ import model
 h = model.Linear()
 
 x = [19,300,430,770,1200]
-y = [0.2885,1.5642,2.4222,3.8497,5.9909]
+y = [2885,15642,24222,38497,59909]
 
 size = len(x)
 
@@ -13,5 +13,6 @@ for epoch in range(500):
         loss = h.MSE(prediction, y[b])
         h.backward()
         h.zero_grad()
-    print("epoch{0}:{1}".format(epoch, h.h_0))
+    if (epoch % 10) == 0:
+        print("epoch{0}:{1}".format(epoch, h.h_0))
 h.summary()
